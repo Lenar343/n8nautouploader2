707db2056ffe0db8b86dc86500ca2170dedab75c
Lenar343 <lenar345alt@gmail.com>
2025-05-28 13:08:53 +0200

    Update script.js new URL

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     
     try {
-      const response = await fetch('https://lenalt343.app.n8n.cloud/webhook/n8n-auto-posting-data', {
+      const response = await fetch('https://lenot344.app.n8n.cloud/webhook/n8n-auto-posting-data', {
         method: 'POST',
         body: formData
       });
